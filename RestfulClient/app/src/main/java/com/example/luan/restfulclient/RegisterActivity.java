e32c8c6cb77edb2ff18474aa6eced5598e9828cf
Luan <vovanluan201294@gmail.com>
2016-03-29 22:49:05 +0700

    add gson lib

--- RestfulClient/app/src/main/java/com/example/luan/restfulclient/RegisterActivity.java
+++ RestfulClient/app/src/main/java/com/example/luan/restfulclient/RegisterActivity.java
@@ -2,9 +2,9 @@ package com.example.luan.restfulclient;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
